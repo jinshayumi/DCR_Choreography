eeda5040a2a594371b3e92b99b9d39cd2f9bca7a
jinshayumi <791682161@QQ.COM>
2022-01-28 19:41:45 +0100

    Delete DCR Choreography/Projection directory

--- DCR Choreography/Projection/src/main/java/checker/ProjectableChecker.java
+++
@@ -1,9 +0,0 @@
-package checker;
-
-import model.DCRGraph;
-
-public class ProjectableChecker {
-    public static boolean isProjectable(DCRGraph dcrGraph, String role){
-        return dcrGraph.endPointProjectable(role);
-    }
-}
--- DCR Choreography/Projection/src/main/java/model/utilities/DCRGraphHelper.java
+++
@@ -1,5 +0,0 @@
-package model.utilities;
-
-public class DCRGraphHelper {
-
-}
